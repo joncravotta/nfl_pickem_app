614d27deeb4f112943c7a8c4465da9f84de73b99
joncravotta <joncravotta32@gmail.com>
2017-09-23 10:33:06 -0400

    Initial Commit

---
+++ nfl_weekly_pickem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nfl_weekly_pickem
+//
+//  Created by Jonathan Cravotta on 9/23/17.
+//  Copyright © 2017 Jonathan Cravotta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
